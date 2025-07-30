136494b3a0615f22bf06d355618fce766719e694
lukasdemani <demani.lukas@gmail.com>
2025-07-30 14:59:31 +0200

    mock

--- backend/app/modules/AppModule.scala
+++ backend/app/modules/AppModule.scala
@@ -1,30 +1,25 @@
 package modules
 
 import com.google.inject.AbstractModule
-import io.getquill.jdbczio.Quill
-import io.getquill.{PostgresZioJdbcContext, SnakeCase}
-import repositories.{UserRepository, UserRepositoryLive}
-import services.{AuthService, AuthServiceLive}
-import zio._
-import javax.sql.DataSource
+import repositories.{UserRepository, UserRepositoryLive, VocabularyRepository}
+import services.{AuthService, AuthServiceLive, VocabularyService, VocabularyServiceImpl, OpenAIService, OpenAIServiceImpl}
+import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
 
 class AppModule extends AbstractModule {
 
   override def configure(): Unit = {
-    // Configure Quill context
-    val quillLayer = Quill.Postgres.fromNamingStrategy(SnakeCase)
+    // Configure Pekko ActorSystem
+    val actorSystem = ActorSystem(Behaviors.empty, "german-learning-system")
+    bind(classOf[ActorSystem[_]]).toInstance(actorSystem)
     
-    // Configure repositories
-    val userRepositoryLayer = UserRepositoryLive.layer
-    
-    // Configure services  
-    val authServiceLayer = AuthServiceLive.layer
-    
-    // Complete layer composition
-    val appLayer = quillLayer >>> userRepositoryLayer >>> authServiceLayer
-    
-    // Bind implementations
+    // Bind repositories
     bind(classOf[UserRepository]).to(classOf[UserRepositoryLive])
+    // VocabularyRepository is a concrete class, not interface
+    
+    // Bind services
     bind(classOf[AuthService]).to(classOf[AuthServiceLive])
+    bind(classOf[VocabularyService]).to(classOf[VocabularyServiceImpl])
+    bind(classOf[OpenAIService]).to(classOf[OpenAIServiceImpl])
   }
 }
